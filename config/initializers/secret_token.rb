07e99623181853f6bdae1f627c10009450a2b2b7
ugorelik <uri.gore@gmail.com>
2013-11-09 23:20:16 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MandrillMailer::Application.config.secret_key_base = '8337758da963a06f92531445796a636e5dc0f195a6ac452df20c86c7e10b74de8e9982606c7c5ab317ac1a7cfbccdfe760c810820320c69e342194f97b96f445'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MandrillMailer::Application.config.session_store :cookie_store, key: '_mandrill_mailer_session'
